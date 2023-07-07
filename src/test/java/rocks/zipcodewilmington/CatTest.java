04c9c4063844ba5abf0d6057f37e5039f7e3f60e
Christine <christine@Zipcoders-MacBook-Pro-51.local>
2023-07-07 09:42:51 -0400

    wrote cat tests and they all pass

--- src/test/java/rocks/zipcodewilmington/CatTest.java
+++ src/test/java/rocks/zipcodewilmington/CatTest.java
@@ -3,6 +3,8 @@ package rocks.zipcodewilmington;
 import org.junit.Assert;
 import org.junit.Test;
 import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.Animal;
+import rocks.zipcodewilmington.animals.Mammal;
 
 import java.util.Date;
 
@@ -40,4 +42,119 @@ public class CatTest {
         Assert.assertEquals(givenId, retrievedId);
     }
 
+    @Test
+    public void testSetName() {
+        // Given (cat and expected output)
+        String expected = "ewwww";
+        Cat cat = new Cat("", new Date(), 0);
+
+        // When (name changes)
+        cat.setName(expected);
+
+        // When (we retrieve data from the cat)
+        String retrievedName = cat.getName();
+
+        // Then (we expect the given data, to match the retrieved data)
+        Assert.assertEquals(expected, retrievedName);
+    }
+
+    @Test
+    public void testSetBirthDate() {
+        // Given (cat and expected output)
+        Date expected = new Date(2015, 05, 26);
+        Cat cat = new Cat("", new Date(), 0);
+
+        // When (birthdate changes)
+        cat.setBirthDate(expected);
+
+        // When (we retrieve data from the cat)
+        Date retrievedBirthDate = cat.getBirthDate();
+
+        // Then (we expect the given data, to match the retrieved data)
+        Assert.assertEquals(expected, retrievedBirthDate);
+    }
+
+    @Test
+    public void testSpeak() {
+        // Given (cat and expected)
+        String expected = "meow!";
+        Cat cat = new Cat("", new Date(), 0);
+
+        // When (we make the cat speak)
+        String result = cat.speak();
+
+        // Then (we expect the given data, to match the retrieved data)
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testEat() {
+        // Given (cat and expected value)
+        Integer expectedMealsEaten = 1;
+        Cat cat = new Cat("", new Date(), 0);
+
+        // When (we make the cat eat)
+        cat.eat(new Food());
+
+        // When (we retrieve the meals eaten)
+        Integer retrievedMealsEaten = cat.getNumberOfMealsEaten();
+
+        // Then (we expect the given data, to match the retrieved data)
+        Assert.assertEquals(expectedMealsEaten, retrievedMealsEaten);
+    }
+
+    @Test
+    public void testGetID1() {
+        // Given (cat data)
+        Integer expectedID = 0;
+
+        // When (a cat is constructed)
+        Cat cat = new Cat("", new Date(), expectedID);
+
+        // When (get the ID of the cat)
+        Integer retrievedID = cat.getId();
+
+        // Then (we expect the given data, to match the retrieved data)
+        Assert.assertEquals(expectedID, retrievedID);
+    }
+    @Test
+    public void testGetID2() {
+        // Given (cat data)
+        Integer expectedID = 13;
+
+        // When (a cat is constructed)
+        Cat cat = new Cat("", new Date(), expectedID);
+
+        // When (get the ID of the cat)
+        Integer retrievedID = cat.getId();
+
+        // Then (we expect the given data, to match the retrieved data)
+        Assert.assertEquals(expectedID, retrievedID);
+    }
+
+    @Test
+    public void testAnimalInheritance() {
+        // Given (cat)
+        Cat cat = new Cat("", new Date(), 0);
+        boolean expectedIsCatAnimal = true;
+
+        // When (get the ID of the cat)
+        boolean isCatAnimal = cat instanceof Animal;
+
+        // Then (we expect the given data, to match the retrieved data)
+        Assert.assertEquals(expectedIsCatAnimal, isCatAnimal);
+    }
+
+    @Test
+    public void testMammalInheritance() {
+        // Given (cat)
+        Cat cat = new Cat("", new Date(), 0);
+        boolean expectedIsCatMammal = true;
+
+        // When (get the ID of the cat)
+        boolean isCatMammal = cat instanceof Mammal;
+
+        // Then (we expect the given data, to match the retrieved data)
+        Assert.assertEquals(expectedIsCatMammal, isCatMammal);
+    }
 }
