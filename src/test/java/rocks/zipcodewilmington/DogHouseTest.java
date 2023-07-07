47231995e5fd6e9a12c40131e3a13fb7f46dfcb0
Christine <christine@Zipcoders-MacBook-Pro-51.local>
2023-07-07 14:06:52 -0400

    made the doghouse test and it's fully functional

--- src/test/java/rocks/zipcodewilmington/DogHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/DogHouseTest.java
@@ -1,5 +1,6 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
 import org.junit.Test;
 import rocks.zipcodewilmington.animals.Dog;
 import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
@@ -30,5 +31,65 @@ public class DogHouseTest {
 
         // Then
         DogHouse.getNumberOfDogs();
+    }
+
+    @Test
+    public void testAddDog(){
+        // Given: a dog
+        Dog doggo = AnimalFactory.createDog("Peppy", new Date());
+        Integer doggoID = doggo.getId();
+
+        // When: you add dog to the house
+        DogHouse.clear();
+        DogHouse.add(doggo);
+
+        // Then: can you get dog from house
+        Assert.assertEquals(doggo, DogHouse.getDogById(doggoID));
+    }
+
+    @Test
+    public void testRemoveDogByID(){
+        // Given a dog in a doghouse
+        Dog doggo = AnimalFactory.createDog("Peppy", new Date());
+        Integer doggoID = doggo.getId();
+        DogHouse.clear();
+        DogHouse.add(doggo);
+
+        // When: you remove cat from house based on ID
+        DogHouse.remove(doggoID);
+
+        // Then: can you get cat from house
+        Assert.assertEquals(null, DogHouse.getDogById(doggoID));
+    }
+
+    @Test
+    public void testRemoveDogByObject(){
+        // Given: a dog in a doghouse
+        Dog doggo = AnimalFactory.createDog("Peppy", new Date());
+        Integer doggoID = doggo.getId();
+        DogHouse.clear();
+        DogHouse.add(doggo);
+
+        // When: you remove dog from house based on doggo
+        DogHouse.remove(doggo);
+
+        // Then: can you get dog from house
+        Assert.assertEquals(null, DogHouse.getDogById(doggoID));
+    }
+
+    @Test
+    public void testGetDogByID(){
+        // Given: a dog in a doghouse
+        Dog doggo = AnimalFactory.createDog("Peppy", new Date());
+        Integer doggoID = doggo.getId();
+        DogHouse.clear();
+        DogHouse.add(doggo);
+
+        // When: you add cat to the house
+        DogHouse.add(doggo);
+        Dog retrievedDog = DogHouse.getDogById(doggoID);
+
+        // Then: can you get cat from house
+        Assert.assertEquals(doggo, retrievedDog);
     }
 }
